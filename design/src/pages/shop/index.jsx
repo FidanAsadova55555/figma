3d939d097fe445e62986e29e216c11ed9e9bffee
FidanAsadova55555 <fidanra@code.edu.az>
2025-03-06 18:35:07 +0400

    some improvements

--- design/src/pages/shop/index.jsx
+++ design/src/pages/shop/index.jsx
@@ -186,7 +186,11 @@ colors
 
 </div>
           </div>
-          <Search onSearch={setSearchTerm} />
+          <Search onSearch={(value) => {
+  setSearchTerm(value);
+  setColors('');
+  setCategories('');
+}} />
 
        <div className='flex flex-col items-center justify-start gap-[80px]'>
        <div className="grid xl:grid-cols-3 grid-cols-2  gap-[12px] lg:gap-[24px]">
